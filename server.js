5006e4a37cd90c299a058e19099da00f646233e7
smart <smart@no-reply.com>
2024-02-21 03:50:08 +0100

    update as global binidng

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0'; // Listen on all network interfaces
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/hello') {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  }else if (req.url === '/hello') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!\n');
   } else if (req.url === '/crash') {
-    // This will cause an uncaught exception and crash the server
+    // Intentionally throw an error to simulate a crash
     throw new Error('Server crash simulated!');
   } else {
     res.statusCode = 404;
